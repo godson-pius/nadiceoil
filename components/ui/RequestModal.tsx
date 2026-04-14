47e959d5b2d6728703a4f3c0f948efbacde7c3b2
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2026-04-14 12:40:27 +0100

    added zIndex for close button

--- components/ui/RequestModal.tsx
+++ components/ui/RequestModal.tsx
@@ -99,7 +99,7 @@ const RequestModal = ({ isOpen, onClose }: RequestModalProps) => {
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                         onClick={onClose}
-                        className="absolute inset-0 bg-black/60 backdrop-blur-md"
+                        className="absolute inset-0 bg-black/60 backdrop-blur-md z-50"
                     />
 
                     {/* Modal Container */}
