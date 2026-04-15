414a625d22182b9fa55fe220314356ed622c43b1
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2026-04-15 14:33:02 +0100

    reduced padding vertical on mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
     }, [])
 
     return (
-        <main className='flex flex-col lg:flex-row items-center px-6 md:px-20 lg:px-44 py-10 gap-10'>
+        <main className='flex flex-col lg:flex-row items-center px-6 md:px-20 lg:px-44 md:py-10 gap-10'>
             <section className='flex flex-col items-center lg:items-start text-center lg:text-left'>
                 <div className='w-max p-1 border-2 border-orange-200 rounded-full hover:scale-105 transition-all duration-700 cursor-pointer hover:shadow' data-cursor="hover">
                     <div className='border-2 w-max font-medium px-4 md:px-6 py-2 text-xs md:text-sm border-orange-300 rounded-full'>Nadice Oil and Gas Limited</div>
